d13eaa0a89fa6ccb42150e944a9a2c32e6189120
Shaun Thompson <shaunthompson@fullsail.edu>
2013-07-15 18:47:02 -0400

    Working along.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 // Shaun Thompson
 // ASDI 1307
+
+/* this is for Web usage not MOBILE
+$(document).ready(function(){
+	$('#somelink').on("click", myFn);
+});
+*/
+
+// Run DOM Check
+$(document).on('pageinit', function(){
+	//all actual code goes here
+	
+
+	
+	
+	
+});
+
+// Create, Read, Delete first --- work on Edit last.
+// AJAX Call - Copy & Paste (from FS) -- pick 2 formats (json & xml?)
+// should just be a button that is clicked and pulls in the above information
