c4089528267606870a3837220e47c6ca149a2d66
Shaun Thompson <shaunthompson@fullsail.edu>
2013-07-18 22:08:02 -0400

    Fixed Local Storage Issue. Having Multi-Display issue, going to move on to AJAX and come back.

--- js/main.js
+++ js/main.js
@@ -3,70 +3,9 @@
 
 // DOM CHECK
 $(document).on('pageinit', function(){
-//all actual code goes here
-
-
-
-	// PREDEFINED VARIABLE IN LOCAL STORAGE
-	
-	/*	var meals = {
-		name: "Orange",
-		date: "2013-05-21",
-		type: "Dinner",
-		calories: "120"
-	};
-	
-	localStorage.setItem("meals", JSON.stringify(meals));
-*/	
-	
-	
-	
-	
-	//Auto Populate from JSON
-	function jsonFill(){
-		for(var n in json){
-			var id = Math.floor(Math.random()*1000000001);
-			localStorage.setItem(id, JSON.stringify(json[n]));
-		};
-	};	
-	
-	//Auto Populate from XML
-	function xmlFill(){
-		for(var n in json){
-			var id = Math.floor(Math.random()*1000000001);
-			localStorage.setItem(id, JSON.stringify(json[n]));
-		};
-	};		
-
-	//Auto Populate from CSV
-	function csvFill(){
-		for(var n in json){
-			var id = Math.floor(Math.random()*1000000001);
-			localStorage.setItem(id, JSON.stringify(json[n]));
-		};
-	};	
-	
-
-
-
-
-
-
-
-
-
-	//	var viewstorage = localStorage.getItem("meals");
-	//	meals = JSON.parse(viewstorage); //var test is now re-loaded!
-
-
-	
-	
-	
-	
-	
 	
 	//ajax parse
-	$(function(){
+/*	$(function(){
 		$.ajax({
    			url      : "xhr/json.php",
 			type     : "GET",
@@ -79,196 +18,69 @@ $(document).on('pageinit', function(){
    			}
    		});
 	});
-	
-	
-	
-	
+*/
+
+
+
+
+	// SUBMIT TO LOCAL STORAGE
+	$('#submitMeal').on("click", storeData(this.key));
+
+		function storeData(key){
+		if(!(key)){
+			var id			= Math.floor(Math.random()*1000000001);
+		}else{
+			id = key;
+		}
+		var meals			= {};
+			meals.name		= $('#name').val();
+			meals.date		= $('#date').val();
+			meals.calories	= $('#calories').val();
+		
+		localStorage.setItem(id, JSON.stringify(meals));
+		
+		//localStorage.meals = JSON.stringify(meals);
+
+		alert("Meal Saved!");
+		return;
+	};	
 	
 	// DISPLAY ITEM
-
-	var value = [1, 2, 3, 4, 5];
-	
 	$("#display").on("click", function(){
-	
-	
-		$('#list').append('<ul></ul>');
-		
-		for(i=0;i<value.length;i++) {
+		if(localStorage.length === 0){
+			alert("There is no information to display. Please select a file for auto-population.");
+			console.log(localStorage.length);
+		}
+		else{
+			$('#list').append('<ul></ul>');
 			
-			$('#list ul').append('<li>' + value[i] + '</li>');
-			}
+			for(var i=0, len=localStorage.length; i<len;i++){
+				var key = localStorage.key(i);
+				$('#list').append('<li>' + localStorage[key] + '</li>');
 			
-		console.log("Before: " + localStorage.length);
-	
-		console.log(localStorage);
-		
-		console.log("After: " + localStorage.length);
+			};
+		};
 	});	
 
 
 
 
 
-	// PARSE LOCAL STORAGE
-/*	function getData(){
-		if(localStorage.length === 0){
-			alert("There are no meals currently tracked so default information has been loaded.");
-			autoFill();
-		};
-		
 
-		var makeDiv = document.createElement('div');
-		makeDiv.setAttribute("id", "items");
-		
-		var makeList = document.createElement('ul');
-		makeDiv.appendChild(makeList);
-		
-		document.body.appendChild(makeDiv);
-		
-		$('items').style.display = "block";
-		
-		
-		for(var i=0, len=localStorage.length; i<len;i++){
-			var makeli = document.createElement('li');
-			var linksLi = document.createElement('li');
-			makeList.appendChild(makeli);
-			var key = localStorage.key(i);
-			var value = localStorage.getItem(key);
-			
-			//Convert the string from local storage
-			var obj = JSON.parse(value);
-			var makeSublist = document.createElement('ul');
-			makeli.appendChild(makeSublist);
-			
-
-			
-			for(var n in obj){
-				var makeSubli = document.createElement('li');
-				makeSublist.appendChild(makeSubli);
-				var optSubText = obj[n][0]+" "+obj[n][1];
-				makeSubli.innerHTML = optSubText;		
-				makeSublist.appendChild(linksLi);
-			};
-			//Create our edit & delete buttons for local storage
-			makeItemLinks(localStorage.key(i), linksLi);
-		};
-	};	*/
-	
-	
-	
-	// original display items
-	/*	var showMeals = function(){
-  	if(meals.length>0){
-		for(var i=0, len=meals.length; i<len; i++){
-    	var newLi = document.createElement('li');
-    		
-	document.getElementById('list').appendChild(newLi);
-  
-      var heading = document.createElement('h3');
-          heading.innerHTML = meals[i].name;
-          	newLi.appendChild(heading);
-      var pDate = document.createElement('p');
-          	pDate.innerHTML = meals[i].date;
-          newLi.appendChild(pDate);
-      var pType = document.createElement('p');
-          pType.innerHTML = meals[i].type;
-          	newLi.appendChild(pType);
-		var pCal = document.createElement('p');
-          pCal.innerHTML = meals[i].calories;
-          	newLi.appendChild(pCal);
-    
-    console.log("Show function completed.");
-
-  }
-  	}
-  	else{
-  		alert("Nothing to show!");
-  		};
-  };
-
-	var display = document.querySelector("#display");
-	display.addEventListener("click", showMeals);
-*/	
-	
-	
-	
-	
-	
-	
-	
-
-
-
-
-	// SUBMIT TO LOCAL STORAGE
-	
-	var saveMeal = function(){
-		var id 				= Math.floor(Math.random()*1000000001);
-		var meals			= {};
-			meals.name		= $('#name').val();
-			meals.date		= $('#date').val();
-			meals.type		= $('#type').val();
-			meals.calories	= $('#calories').val();
-		
-		localStorage.setItem(id, JSON.stringify(meals));
-
-				alert("Meal Saved!");
-	};	
-	
-	$("#submitMeal").click(saveMeal);
-
-	//  submit item original event handler 
-	/*  		
-		var saveMeal = function(){
-		
-			var newMeal = {};
-
- 			newMeal.name = $('name').value;
-  			newMeal.date = $('date').value;
-  			newMeal.type = $('type').value;
-  			newMeal.calories = $('calories').value;
-  
-
-		location.href="#home";
-		$('list').innerHTML = "";
-	
-		console.log("Save function completed.");  
-		console.log(newMeal);
-
-	};
-
-	var save = document.querySelector("#submitMeal");
-	save.addEventListener("click", saveMeal);
-*/
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	// CLEAR STORAGE
-	var clearMeals = function(){
-		if(localStorage.length===0){
-			alert("Already Cleared!")
+	$('#clear').on("click", function(){
+		if(localStorage.length === 0){
+			alert("Already Cleared!");
+			return;
 		}
 		else{
 			localStorage.clear();
-  	
+  			alert("All Data Cleared.");
 			console.log("Clear function completed.");
 		};
-	};
+	});
 
-	$("#clear").click(clearMeals);
-	
-	
-	
 	
 	
 	
